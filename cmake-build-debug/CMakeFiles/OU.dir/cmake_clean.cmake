d5276b50d0ee797a76180f9fc840939d3cf09bb7
Bartizard <252331@edu.p.lodz.pl>
2023-11-19 14:49:07 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU.dir/main.cpp.o"
+  "OU"
+  "OU.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
